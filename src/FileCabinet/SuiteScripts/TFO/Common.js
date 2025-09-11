036fc7a3666d0d17afc89b59dd6ca1ee821ecad4
Grant Le Roux <cram42@users.noreply.github.com>
2025-09-11 10:45:21 +0800

    Cleanup and doc

--- src/FileCabinet/SuiteScripts/TFO/Common.js
+++ src/FileCabinet/SuiteScripts/TFO/Common.js
@@ -10,18 +10,21 @@ define(
     (Args, Arrays, Search) => {
 
         return {
+            /* Modules */
             Args: Args,
             Search: Search,
 
+            /* Args */
+            parseArgs: Args.parse,
+
+            /* Arrays */
             findByProperty: Arrays.findByProperty,
             groupByProperty: Arrays.groupByProperty,
 
-            parseArgs: Args.parse,
-
+            /* Search */
             SearchOperator: Search.Operator,
             SearchParsing: Search.Parsing,
             SearchType: Search.Type,
-            
             createSearchColumn: Search.createColumn,
             getSingleField: Search.getSingleField,
             simpleSearch: Search.simpleSearch,
